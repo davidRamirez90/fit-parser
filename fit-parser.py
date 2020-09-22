90af403872b24b8d250d114bd0959dfb48c115d8
David Ramirez <david.ramirez@motionminers.de>
2020-09-22 14:54:06 +0200

    [chg] exporting several activities in a single format json

--- fit-parser.py
+++ fit-parser.py
@@ -1,34 +1,53 @@
 import fitdecode
 import json
+import numpy as np
+import os
 
-def saveJson(obj, name):
-    with open('{name}.json'.format(name=name), 'w') as outfile:
+def saveJson(obj):
+    with open('activities.json', 'w') as outfile:
         json.dump(obj, outfile)
 
-def decodeFitFile(file, field, keys):
-
-    obj = {
-        'distance': [],
-        'cadence': [],
-        'power': []
-    }
+def decodeFitFile(file, name):
+    power = []
+    obj = {}
     with fitdecode.FitReader(file) as fit:
         for frame in fit:
             if isinstance(frame, fitdecode.FitDataMessage):
-                if (frame.name == field):
-                    for k in keys:
-                        obj[k].append(frame.get_value(k))
+                if frame.name == 'file_id':
+                    if frame.has_field('time_created'):
+                        obj['date'] = frame.get_value('time_created').isoformat()
+                if frame.name == 'record':
+                    if frame.has_field('power'):
+                        power.append(frame.get_value('power'))
+
+    zones, _ = np.histogram(np.array(power), bins=[0, 176, 220, 2000])
+    obj['zones'] = zones.tolist()
+    obj['samples'] = len(power)
+    obj['name'] = name.replace('.fit', '')
+
+    print(zones)
+
 
     return obj
 
 def main():
-    name = '2020-09-20-135543-ELEMNT BOLT EFFF-21-0'
-    url = "./{name}.fit".format(name=name)
-    field_name = 'record'
-    keys = ['distance', 'cadence', 'power']
-    obj = decodeFitFile(url, field_name, keys)
+    path = './fit_files'
+    files = [f for f in os.listdir(path) if f.endswith('.fit')]
 
-    saveJson(obj, name)
+    print(files)
+
+    myJson = {
+        'activities': []
+    }
+
+    for file in files:
+
+        url = "./fit_files/{name}".format(name=file)
+        obj = decodeFitFile(url, file)
+        myJson['activities'].append(obj)
+        print('Saved file {}'.format(obj['name']))
+
+    saveJson(myJson)
     print('success')
 
 
